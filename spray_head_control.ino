598a144a1bbbeebe64df21ccf9e8ec980d95a7f6
CalicatSJL <stevenlewis@calicat.com>
2025-09-08 17:37:22 -0700

    Add files via upload

---
+++ spray_head_control.ino
@@ -0,0 +1,44 @@
+#include <Servo.h>
+
+Servo SprayHead;
+
+// Analog input pin
+int M106_Input_Pin = A5;      // Controls Servo and LED
+
+// Output pins
+int servoPin = 10;      // Servo control pin
+
+// Servo angles
+int A_ON = 60;
+int A_OFF = 0;
+
+// Sensor values
+float M106_Input_Reading = 0;
+float M106_Input_Voltage = 0;
+
+int delayValue = 10;
+
+void setup() {
+  Serial.begin(9600);
+  SprayHead.attach(servoPin);
+  SprayHead.write(A_ON); // Start in OFF position
+}
+
+void loop() {
+  // Read analog voltage from A5
+  M106_Input_Reading = analogRead(M106_Input_Pin);
+  M106_Input_Voltage = (M106_Input_Reading / 1023.0) * 5.0; //scale 0-5V
+
+  // Print sensor voltage for debugging
+  //Serial.print("Servo Sensor Voltage (A5): ");
+  //Serial.println(voltage_sense, 3);
+
+  // Control spray head and LED
+  if (M106_Input_Voltage > 3) {
+    SprayHead.write(A_OFF);   // Spray OFF
+  } else {
+    SprayHead.write(A_ON);     // Spray ON
+  }
+
+  delay(delayValue); // Wait before next reading
+}
